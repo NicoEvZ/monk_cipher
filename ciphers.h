c862f14ce6ad0218d815ba11a57d609bdf9fa2d6
Nico van Zyl <nevanzyl@gmail.com>
2025-09-16 22:34:17 +1200

    refactor files for correct importing

--- ciphers.h
+++ ciphers.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <stdint.h>
-
 #define cipher_len 35
 #define cipher_width 5
 #define cipher_height 7
@@ -15,9 +13,16 @@
 #define hundreds_place_fragment_start_index 23
 #define thousands_place_fragment_start_index 20
 
-extern const uint8_t bin_cipher_1234[cipher_height];
-extern const uint8_t bin_cipher_fragments[10];
-extern const char cipher_fragments[10][fragment_len];
+// #define PRINT_CHARACTER "\u25cf" //circle
+// #define PRINT_CHARACTER "\u25a0 " //Black Square + space
+#define PRINT_CHARACTER "\u25fc " //Black Square
+// #define PRINT_ON_COLOUR "\e[95m" // bright pink foreground
+#define PRINT_ON_COLOUR "\e[93m" // bright yellow foreground
+#define PRINT_BG_COLOUR "\e[43m" //yellow BG
+#define PRINT_OFF_COLOUR "\e[90m" // bright black foreground
+// #define PRINT_OFF_COLOUR "\e[30m" // dark black foreground
+// #define PRINT_CHARACTER "\u25fc " //Black Mediun Square
+// #define PRINT_CHARACTER "\u25fb " //White Mediun Square
 
 typedef struct four_digit_place_values
 {
@@ -35,6 +40,11 @@ typedef struct quad_display
 {
     cipher_t *display_one, *display_two, *display_three, *display_four;
 }quad_display_t;
+
+void display_quad_ciphers_bin(quad_display_t * quad_display);
+
+void clear_quad_display(quad_display_t * display_to_clear);
+
 
 
 
